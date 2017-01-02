e571e2f80134cf3f72e0ed4d3a514c5daa127b13
Mark van de Korput <dr.theman@gmail.com>
2017-01-02 17:30:57 +0100

    use camera’s orientation to colorize rings

--- sketches/cylinders/scripts/shader1.py
+++ sketches/cylinders/scripts/shader1.py
@@ -1,17 +1,22 @@
 import bge
 
 def apply(controller):
-    mesh = controller.owner.meshes[0]
+    owner = controller.owner
+    mesh = owner.meshes[0]
 
     for mat in mesh.materials:
         shader = mat.getShader()
         if shader != None:
             if not shader.isValid():
                 shader.setSource(VertexShader, FragmentShader, 1)
+            colorobj = owner.scene.objects[owner['colorobj']] if 'colorobj' in owner else owner
+            xyz = colorobj.localOrientation.to_euler()
+            shader.setUniform3f('customColor', xyz.x, xyz.y, xyz.z)
 
 VertexShader = """
     // varying vec4 color;
     varying vec4 texCoords;
+
 
     void main() // all vertex shaders define a main() function
     {
@@ -24,10 +29,11 @@ VertexShader = """
 FragmentShader = """
     // varying vec4 color;
     varying vec4 texCoords;
+    uniform vec3 customColor;
 
     void main()
     {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0-abs(texCoords.y-0.5)*1.7);
+        gl_FragColor = vec4(customColor, 1.0-abs(texCoords.y-0.5)*1.7);
         // gl_FragColor = vec4(1.0, 0.0, 0.0, texCoords.x);
         // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }
